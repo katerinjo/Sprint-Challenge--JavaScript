7bfee16323126f8968f01eae93394a43cfebff26
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-06-14 09:19:24 -0700

    request 3

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -94,7 +94,7 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = universities.filter(university => university.includes('Uni'));
 console.log(uni);
 
 
